0b2ea6ee265356896616b13f9fb8482b19751310
Nitish-Repo <nitishkumar9876542gmail.com>
2025-12-03 16:54:48 +0530

    feat(private-layout): enhance footer visibility logic and improve router event handling

--- src/app/layouts/private/private-layout.page.ts
+++ src/app/layouts/private/private-layout.page.ts
@@ -1,17 +1,18 @@
-import { Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonRouterOutlet } from '@ionic/angular/standalone';
 import { SidemenuPage } from './sidemenu/sidemenu.page';
 import { FooterPage } from './footer/footer.page';
 import { AppSettingService } from '@app/core/app-setting.service';
 import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
+import { filter, tap } from 'rxjs';
 
 @Component({
   selector: 'app-private-layout',
   templateUrl: './private-layout.page.html',
   styleUrls: ['./private-layout.page.scss'],
   standalone: true,
+  // changeDetection:ChangeDetectionStrategy.OnPush,
   imports: [
     CommonModule,
     IonRouterOutlet,
@@ -25,6 +26,10 @@ export class PrivateLayoutPage {
 
   isDesktop = signal<boolean>(false);
   hideFooter = signal<boolean>(false);
+  showFooter = computed(() =>{
+    return !(this.isDesktop() || this.hideFooter())
+  })
+
 
   hideFooterUrls: string[] = ['/products', '/customers'];
 
@@ -34,12 +39,15 @@ export class PrivateLayoutPage {
 
     this.isDesktop.set(desktop);
 
-     this.router.events
-      .pipe(filter(event => event instanceof NavigationEnd))
+    // Check initial URL
+    this.updateFooterVisibility(this.router.url);
+
+    // Listen for navigation changes
+    this.router.events
+      .pipe(tap(console.log), filter(event => event instanceof NavigationEnd))
       .subscribe((event: NavigationEnd) => {
         this.updateFooterVisibility(event.urlAfterRedirects);
       });
-
   }
 
    private updateFooterVisibility(url: string) {
