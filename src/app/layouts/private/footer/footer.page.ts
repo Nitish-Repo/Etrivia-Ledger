645afc59705de3566bec1d0efdaa0429f99fefdd
Nitish-Repo <nitishkumar9876542gmail.com>
2025-12-04 09:07:13 +0530

    refactor(private-layout): remove unused ngAfterViewInit method and debugging info from footer

--- src/app/layouts/private/footer/footer.page.ts
+++ src/app/layouts/private/footer/footer.page.ts
@@ -29,7 +29,7 @@ import {
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class FooterPage implements AfterViewInit {
+export class FooterPage {
   selectedTab = signal<string>('home');
   private tabs = ['home', 'dashboard', 'sell'];
 
@@ -37,28 +37,28 @@ export class FooterPage implements AfterViewInit {
     addIcons({ home, homeOutline, grid, gridOutline, cube, cubeOutline, cart, cartOutline });
   }
 
-  ngAfterViewInit() {
-    setTimeout(() => {
-      const el = document.querySelector('ion-router-outlet');
-      if (el) {
-        this.gestureCtrl.create({
-          el, gestureName: 'swipe-tabs', threshold: 15, direction: 'x',
-          onEnd: (d) => {
-            if (Math.abs(d.deltaX) > Math.abs(d.deltaY) && Math.abs(d.deltaX) > 50) {
-              const i = this.tabs.indexOf(this.selectedTab());
-              if (d.deltaX > 0 && i > 0) this.nav(i - 1);
-              else if (d.deltaX < 0 && i < this.tabs.length - 1) this.nav(i + 1);
-            }
-          }
-        }, true).enable();
-      }
-    }, 500);
-  }
+  // ngAfterViewInit() {
+  //   setTimeout(() => {
+  //     const el = document.querySelector('ion-router-outlet');
+  //     if (el) {
+  //       this.gestureCtrl.create({
+  //         el, gestureName: 'swipe-tabs', threshold: 15, direction: 'x',
+  //         onEnd: (d) => {
+  //           if (Math.abs(d.deltaX) > Math.abs(d.deltaY) && Math.abs(d.deltaX) > 50) {
+  //             const i = this.tabs.indexOf(this.selectedTab());
+  //             if (d.deltaX > 0 && i > 0) this.nav(i - 1);
+  //             else if (d.deltaX < 0 && i < this.tabs.length - 1) this.nav(i + 1);
+  //           }
+  //         }
+  //       }, true).enable();
+  //     }
+  //   }, 500);
+  // }
 
   setCurrentTab(e: any) { this.selectedTab.set(e.tab); }
   
-  private nav(i: number) {
-    this.selectedTab.set(this.tabs[i]);
-    this.router.navigate([`/${this.tabs[i]}`]);
-  }
+  // private nav(i: number) {
+  //   this.selectedTab.set(this.tabs[i]);
+  //   this.router.navigate([`/${this.tabs[i]}`]);
+  // }
 }
