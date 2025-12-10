a3c80fa8b9cfd550066f34cb81b6b35b41905acc
Nitish-Repo <nitishkumar9876542gmail.com>
2025-12-10 17:54:03 +0530

    feat(product): enhance product listing with detailed item view and search functionality

--- src/app/features/components/private/products/products.component.ts
+++ src/app/features/components/private/products/products.component.ts
@@ -4,28 +4,40 @@ import { Router, RouterModule } from '@angular/router';
 import { Product } from '@app/features/models/product.model';
 import { ProductService } from '@app/features/services/product.service';
 import { ToolbarPage } from '@app/layouts/private/toolbar/toolbar.page';
-import { IonHeader, IonContent, IonButton } from "@ionic/angular/standalone";
+import { IonHeader, IonContent, IonButton, IonToolbar, IonSearchbar, IonFab, IonFabButton, IonIcon, IonList, IonLabel, IonItem, IonText, IonNote } from "@ionic/angular/standalone";
+import { addIcons } from 'ionicons';
+import { add, cubeOutline, ellipsisVertical, chevronForward } from 'ionicons/icons';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
   standalone: true,
-  imports: [IonButton, IonContent, IonHeader, CommonModule, ToolbarPage, RouterModule]
+  imports: [IonNote, IonText, IonItem, IonLabel, IonList, IonIcon, IonFabButton, IonFab, IonSearchbar, IonToolbar, IonButton, IonContent, IonHeader, CommonModule, ToolbarPage, RouterModule]
 })
-export class ProductsComponent  implements OnInit {
+export class ProductsComponent implements OnInit {
   private router = inject(Router);
   private productService = inject(ProductService);
 
-  products?: Product[];
+  products: Product[] = [];
+  public results: Product[] = [];
 
-  constructor() { }
+  constructor() {
+    addIcons({cubeOutline,ellipsisVertical,chevronForward,add});
+  }
 
   ngOnInit() {
-    this.productService.getAllProducts().subscribe((x)=>{
-      this.products = x
+    this.productService.getAllProducts().subscribe((x) => {
+      this.products = x;
+      this.results = [...x];
     })
-    
+
+  }
+
+  handleInput(event: Event) {
+    const target = event.target as HTMLIonSearchbarElement;
+    const query = target.value?.toLowerCase() || '';
+    this.results = this.products!.filter((d) => d.productName.toLowerCase().includes(query));
   }
 
 }
